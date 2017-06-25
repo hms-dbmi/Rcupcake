dd4c591d9041e61e0d605703abf146d3eadbc5ea
aGutierrezSacristan <alba.gutierrez@upf.edu>
2017-06-25 17:58:03 +0200

    Vigneete gramatical and ortographic errors fixed

--- R/cupcake-show.R
+++ R/cupcake-show.R
@@ -2,9 +2,9 @@ setMethod( "show",
            signature = "cupcakeData",
            definition = function( object ) {
                cat( "Object of class 'cupcakeData'\n" )
-               cat( " . N. Variations:   ", nrow ( object@variations ) , "\n" )
-               cat( " . N. Phenotypes:  ", nrow ( object@phenotypes ), "\n" )
-               cat( " . N. Patients:    ", length( unique ( object@iresult$patient_id ) ), "\n" )
+               cat( " . Number of Variations:   ", nrow ( object@variations ) , "\n" )
+               cat( " . Number of Phenotypes:   ", nrow ( object@phenotypes ), "\n" )
+               cat( " . Number of Patients:     ", length( unique ( object@iresult$patient_id ) ), "\n" )
            }
 )
 
@@ -17,7 +17,7 @@ setMethod( "show",
                cat( " . Gender  :", object@gender, "\n" )
                cat( " . Variation :", object@variation, "\n" )
                cat( " . Patients in the age and gender interval:", object@patients, "\n" )
-               cat( " . Patients with vairation selected:", object@tpatients, "\n" )
+               cat( " . Patients with variation selected:", object@tpatients, "\n" )
                cat( " . Prevalence:", round(as.numeric(object@prevalence), 3), "\n")
                cat( " . Odds ratio range:", object@ORrange, "\n")
                cat( " . Relative risk range:", object@RRrange, "\n")
