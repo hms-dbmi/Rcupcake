fd13e6bc4e64ce50bccd35146c1454a6236c0891
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2018-03-30 15:05:08 -0400

    Special characters in paths: spaces; solved

--- R/get.children.R
+++ R/get.children.R
@@ -25,7 +25,7 @@ get.children <- function( fieldname, url, verbose = FALSE) {
     IRCT_RESOURCE_BASE_URL <- paste(IRCT_CL_SERVICE_URL,"resourceService/",sep="")
     IRCT_PATH_RESOURCE_URL <- paste(IRCT_RESOURCE_BASE_URL,"path",sep="")
     
-      fieldname <- gsub("\\#","%23", gsub("\\?", "%3F", gsub("[)]","%29", gsub("[(]","%28", fieldname))))
+      fieldname <- gsub("\\#","%23", gsub("\\?", "%3F", gsub("[)]","%29", gsub(" ","%20", gsub("[(]","%28", fieldname)))))
     
     nexturl <- paste( IRCT_PATH_RESOURCE_URL, fieldname, sep = "" )
     newchildren <-  httr::content(httr::GET(nexturl))
@@ -36,7 +36,7 @@ get.children <- function( fieldname, url, verbose = FALSE) {
                 newchild <- newchildren[[i]]$pui
                 children <- c(children, newchild)
                 getchildren(children, 
-                            gsub("\\#","%23", gsub("\\?", "%3F", gsub("[)]","%29", gsub("[(]","%28", URLencode(newchild)))))
+                            gsub("\\#","%23", gsub("\\?", "%3F", gsub("[)]","%29", gsub(" ","%20", gsub("[(]","%28", URLencode(newchild))))))
                 )
             }, error = function(errorCondition) {
                 if( verbose == TRUE){
