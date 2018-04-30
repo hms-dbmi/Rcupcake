dcab06fb815c59f38f47f836f941cf843956ae84
Mikaunix <mikaunix@gmail.com>
2018-04-30 07:23:11 -0400

    concatPath enhancement: accepts arbitrary # of params instead of a single vector

--- R/concatPath.R
+++ R/concatPath.R
@@ -1,6 +1,6 @@
 ## > concatPath(c("a","/b","/c/","/d","e"))
 ## [1] "a/b/c/d/e"
-concatPath <- function(v){
+concatPath <- function(...){
     f <- function(a, b){
         ## count nb of slashes already in place
         n = (if(substring(a, nchar(a))=="/") 1 else 0) + (if(substring(b,1,1)=="/") 1 else 0)
@@ -9,7 +9,6 @@ concatPath <- function(v){
         else if(n==1) paste0(a, b)
         else          paste0(a, substring(b,2))
     }
-    if(length(v)<=1) v
-    else f(v[1], concatPath(v[-1]))
     
+    Reduce(f, unlist(list(...)))
 }
